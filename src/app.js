1981268d13f971b21db9576055c82d6d1002128f
Matias Zabala <zabala@live.com.ar>
2025-08-10 10:16:01 -0300

    Helpers de variables de entorno y argumentos OK

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import {config} from "dotenv"
+import "./helpers/env.helper.js"
 import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
@@ -9,12 +9,16 @@ import adoptionsRouter from './routes/adoption.router.js';
 import sessionsRouter from './routes/sessions.router.js';
 import mocksRouter from './routes/mocks.router.js'
 import errorHandler from './middlewares/errorHandler.js';
+import argsHelper from "./helpers/args.helper.js";
 
-config()
 const app = express();
 const PORT = process.env.PORT||8080;
-console.log(process.env)
-const connection = mongoose.connect(process.env.MONGO_DB)
+const ready = async () => {
+    console.log(`Server ready on port ${PORT}`)
+    console.log("mode" + argsHelper.mode)
+}
+
+const connection = mongoose.connect(process.env.URL_MONGO)
 
 app.use(express.json());
 app.use(cookieParser());
@@ -23,7 +27,7 @@ app.use('/api/users',usersRouter);
 app.use('/api/pets',petsRouter);
 app.use('/api/adoptions',adoptionsRouter);
 app.use('/api/sessions',sessionsRouter);
-app.use('/api/mocks', mocksRouter)
+app.use('/api/mocks', mocksRouter);
 
-app.listen(PORT,()=>console.log(`Listening on ${PORT}`))
+app.listen(PORT, ready, ()=>console.log(`Listening on ${PORT}, mode:${argsHelper.mode}`))
 app.use(errorHandler)
--- src/helpers/env.helper.js
+++ src/helpers/env.helper.js
@@ -1,5 +1,5 @@
 import { config } from "dotenv";
-import argsHelpers from "./args.helpers.js";
+import argsHelpers from "./args.helper.js";
 
 const path = ".env." + argsHelpers.mode
 config({ path })
